fd08cdb00586d1f401e074028fa05a8914888345
sudhanshu7277 <12502126+sudhanshu7277@users.noreply.github.com>
2025-09-18 00:57:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <div>
+          React starter deployment using publish profile approach !
+        </div>
+    </>
+  );
+}
+
+export default App;
